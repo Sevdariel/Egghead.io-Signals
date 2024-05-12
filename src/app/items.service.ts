a1148e7804047360d9facdd5b8db1e7eec17625b
Michał Zalewski <michal.zalewski1995@gmail.com>
2024-05-12 20:20:21 +0200

    Lesson 13

--- src/app/items.service.ts
+++ src/app/items.service.ts
@@ -1,6 +1,11 @@
 import { Injectable, effect, signal, untracked } from '@angular/core';
 import _ from 'lodash';
 
+type Item = {
+  id: number;
+  name: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,11 +16,11 @@ export class ItemsService {
 
   example = signal(123);
 
-  #items = signal([
-    { id: 1, name: 'Andy' },
-    { id: 2, name: 'Bob' },
-    { id: 3, name: 'Charlie' },
-  ],
+  synchronizeItemsEffect = effect(() => {
+    localStorage.setItem('items', JSON.stringify(this.#items()));
+  })
+
+  #items = signal(JSON.parse(localStorage.getItem('items')!) as Array<Item>
     //{ equal: _.isEqual }
     // ], {equal: (prev, curr) => Object.is(prev, curr)})
     // Object.is compares reference
@@ -27,10 +32,6 @@ export class ItemsService {
   }
 
   clearItems() {
-    this.#items.set([
-      { id: 1, name: 'Andy' },
-      { id: 2, name: 'Bob' },
-      { id: 3, name: 'Charlie' },
-    ]);
+    this.#items.set([]);
   }
 }
